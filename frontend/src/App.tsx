130c378f6ca274e0f7cdec1119f49617a57e48cc
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-09-03 18:23:49 -0300

    style: global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import store from './store';
 import Routes from './routes';
+import GlobalStyles from './styles/Global';
 
 const App: React.FC = () => {
   return (
     <PersistGate loading={null} persistor={store}>
       <BrowserRouter>
+        <GlobalStyles />
         <Routes />
       </BrowserRouter>
     </PersistGate>
