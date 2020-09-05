60d8ce493b7e825c9e916d1f343fc7c38edcd521
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-09-04 21:15:19 -0300

    refact: add connected router

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Route from './Route';
+import history from './history';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
-  </Switch>
+  <ConnectedRouter history={history}>
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/signup" exact component={SignUp} />
+    </Switch>
+  </ConnectedRouter>
 );
 
 export default Routes;
